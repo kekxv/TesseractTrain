fa30e026848b3a081373d5c77ef6f38f34301192
caesar <caesar@kekxv.com>
2018-11-17 22:03:37 +0800

    修改部分错误

---
+++ OCR_B/Train/train.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# 进入当前目录：
+cd $(dirname $0)
+
+# 定义输出语言 lang：
+lang=OCR_B
+
+# 获取数据 tr：
+tr=`ls ../exp*/*.tr`
+
+# 获取数据 box：
+box=`ls ../exp*/*.box`
+
+# 生成unicharset文件：
+unicharset_extractor $tr
+
+# 聚类：Clustering:
+shapeclustering -F ../../Data/font_properties -U unicharset $tr
+mftraining -F ../../Data/font_properties -U unicharset -O $lang.unicharset $tr
+cntraining $tr
+
+#合并训练数据
+mv shapetable $lang.shapetable
+mv normproto $lang.normproto
+mv inttemp $lang.inttemp
+mv pffmtable $lang.pffmtable
+combine_tessdata $lang.
+
+rm $lang.inttemp $lang.pffmtable $lang.normproto $lang.shapetable $lang.unicharset unicharset
