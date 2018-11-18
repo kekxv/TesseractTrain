7501d686a8836f6e0f4253c5d94148eff2640b36
caesar <caesar@kekxv.com>
2018-11-18 22:07:48 +0800

    修复说明

--- OCR_B/exp1/run.sh
+++ OCR_B/exp1/run.sh
@@ -13,8 +13,18 @@ num=1
 # 图像处理：
 convert 'image/*' -density 300 ./tif/%d.tif
 
+# jTessBoxEditor
+# 打开 jTessBoxEditor
+../../Tool/jTessBoxEditor/train &
+#提示继续
+read -p "请使用 jTessBoxEditor 合并 .tif,然后按回车继续:" a
+
 # 生成 .box 文件
 tesseract $lang.$fontname.exp$num.tif $lang.$fontname.exp$num batch.nochop makebox
 
-# 生成 .box 文件
-tesseract $lang$.$fontname$.exp$num.tif $lang.$fontname.exp$num box.train
+# 生成 .tr 文件 确定图片可用
+tesseract $lang.$fontname.exp$num.tif $lang.$fontname.exp$num box.train
+#提示继续
+read -p "请使用 jTessBoxEditor 调整 tif 位置,然后按回车继续:" a
+# 生成 .tr 文件 
+tesseract $lang.$fontname.exp$num.tif $lang.$fontname.exp$num box.train
